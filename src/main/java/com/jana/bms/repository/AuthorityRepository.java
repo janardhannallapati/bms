ffd4014226ce6c597aa5d0cfc98f5b9421a0f222
janardhannallapati <janardhan.nallapati@gmail.com>
2022-09-03 17:13:02 +0530

    Initial version of bms with all Spring boot classes (repository, entity, controller) and test data

---
+++ src/main/java/com/jana/bms/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.jana.bms.repository;
+
+import com.jana.bms.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
